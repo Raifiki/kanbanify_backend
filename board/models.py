588b2de0532c17a943c1088e0b56b84cf4cbec8e
Raifikie <leonard_weiss@web.dem>
2024-07-25 17:27:47 +0200

    add null=true at task model for fields which are not requested

--- board/models.py
+++ board/models.py
@@ -22,13 +22,13 @@ class Category(models.Model):
     
 class Task(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(null=True)
     assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
     created_from = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='created_tasks')
     created_at = models.DateTimeField(auto_now_add=True)
     due_date = models.DateTimeField(null=True)
     priority = models.CharField(max_length=10)
-    label = models.CharField(max_length=100)
+    label = models.CharField(max_length=100, null=True)
     board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='tasks')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='tasks')
     
